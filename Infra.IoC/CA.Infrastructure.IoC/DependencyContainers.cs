618666ed74a058b31aff803c506fe7689ecf8474
Mehrdad Jannesar <Jannesar.computer@yahoo.com>
2022-11-24 12:50:45 +0330

    Define base template

---
+++ Infra.IoC/CA.Infrastructure.IoC/DependencyContainers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Infrastructure.IoC
+{
+    public class DependencyContainers
+    {
+
+        // Dependency Injection (In version of control --- IoC)
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Application Layer
+            //service.AddScoped<>();
+
+            //Infrastructure Data Layer
+            //service.AddScoped<>();
+        }
+    }
+}
